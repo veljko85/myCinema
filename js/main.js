ef5713725caa9c0f6043e3ebd11e3acbc85fe5a9
Velibor Markovic <veljko85@users.noreply.github.com>
2019-02-04 19:41:45 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+	
+
+function hamburger() {
+
+	var line1 = document.getElementById("firstLine").style.top;
+	var line2 = document.getElementById("secondLine").style.display;
+	var line3 = document.getElementById("thirdLine").style.top;
+	var navWidth = document.getElementById("nav").style.marginLeft;
+	var homeSection = document.getElementById("homeSection").style.marginTop;
+
+	if (line2 == "none") {
+		document.getElementById("firstLine").classList.remove("change1");
+		document.getElementById("secondLine").style.display = "block";
+		document.getElementById("thirdLine").classList.remove("change2");
+
+	}	else	{
+		document.getElementById("firstLine").classList.add("change1");
+		document.getElementById("secondLine").style.display = "none";
+		document.getElementById("thirdLine").classList.add("change2");
+	}
+	
+	if (navWidth == "-20%") {
+		document.getElementById("nav").style.marginLeft = "-100%";
+	} else	{
+		document.getElementById("nav").style.marginLeft = "-20%";
+	}
+
+	if (homeSection == "0px") {
+		document.getElementById("homeSection").style.marginTop = "-124px";
+	} else	{
+		document.getElementById("homeSection").style.marginTop = "0px";
+	}
+}
