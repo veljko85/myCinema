b65f7c18a1815cf1b0a3b477dfc6a8d2ae5b159d
Velibor Markovic <veljko85@users.noreply.github.com>
2019-02-06 20:15:25 +0100

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$("#nav a").click( function() {
 		$("#homeSection").hide();
-		$("nav").css({"margin-left": "-100%"});		
+		$("#nav").css({"margin-left": "-100%"});
+		$("#content").css({"margin-top": "-124px"});		
 		$("#content").load($(this).attr("href"));
         	return(false);
 	});
